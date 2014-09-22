3b4db4f48db30b290c787fe56ead68cb0804bb51
Tony Kay <tony.kay@gmail.com>
2014-09-22 09:12:04 -0700

    updated spec

--- js/sample.js
+++ js/sample.js
@@ -3,4 +3,17 @@ function distance(p1, p2) {
     var dy = p1.y - p2.y;
     return Math.sqrt(dx * dx + dy * dy);
 }
-//this is a comment...
+
+/*
+This function should return true if the given "word" starts with "beginning"
+
+  e.g.
+  startsWith("Hello", "Hell") -> true
+  startsWith("This is a test", "This") -> true
+  startsWith("My name is Mudd", "Hi") -> false
+ */
+function startsWith(word, beginning) {
+    // HINT: remember that strings are objects with a method substr()...e.g. "hello".substr(3,4)
+    return true;
+}
+
--- specs/sample-spec.js
+++ specs/sample-spec.js
@@ -20,3 +20,19 @@ describe("The distance function", function () {
         expect(distance(a, b)).toBeCloseTo(7.74, 0.01);
     });
 });
+
+describe("The startsWith function", function() {
+    it("returns true when the first parameter starts with the second", function () {
+        expect(startsWith("Hi there", "Hi")).toBe(true);
+        expect(startsWith("Something else", "Some")).toBe(true);
+        expect(startsWith("Boogers are tasty", "Boo")).toBe(true);
+    });
+    it("is case sensitive", function () {
+        expect(startsWith("hi there", "Hi")).toBe(false);
+    });
+    it("returns false when the first parameter does not start with the second", function () {
+        expect(startsWith("Hi there", "there")).toBe(false);
+        expect(startsWith("Something else", "thing")).toBe(false);
+        expect(startsWith("Boogers are tasty", "are")).toBe(false);
+    });
+});
