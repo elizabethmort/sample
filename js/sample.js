a2829a19a5fc0745ef4dba8594226e847ba6e09e
Tony Kay <tony.kay@gmail.com>
2014-09-22 09:15:27 -0700

    added a bit more of a template

--- js/sample.js
+++ js/sample.js
@@ -5,15 +5,20 @@ function distance(p1, p2) {
 }
 
 /*
-This function should return true if the given "word" starts with "beginning"
+ This function should return true if the given "word" starts with "beginning"
 
-  e.g.
-  startsWith("Hello", "Hell") -> true
-  startsWith("This is a test", "This") -> true
-  startsWith("My name is Mudd", "Hi") -> false
+ e.g.
+ startsWith("Hello", "Hell") -> true
+ startsWith("This is a test", "This") -> true
+ startsWith("My name is Mudd", "Hi") -> false
  */
 function startsWith(word, beginning) {
-    // HINT: remember that strings are objects with a method substr()...e.g. "hello".substr(3,4)
-    return true;
+    // HINT: remember that strings are objects with methods like substr()...e.g. word.substr(3,4)
+    var lengthOfBeginning = ?;
+    var startOfWord = ?;
+    if (?)
+        return true;
+    else
+        return false;
 }
 
